7945790862a81a47ae42ef7064d7df1e58dd9ad8
david <david@DESKTOP-SI10PSB>
2017-04-27 23:50:23 -0500

    validacion de correo electronico

---
+++ src/correo/Correo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package correo;
+
+/**
+ *
+ * @author david jimenez - duvan poveda
+ */
+public class Correo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Ventanacorreo ventana = new Ventanacorreo();
+    
+    }
+    
+}
